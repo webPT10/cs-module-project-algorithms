680bf5e425e68679db8a1dc396d5fc745800005d
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-09-07 16:16:56 -0700

    product_of > completed and tested

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -1,16 +1,21 @@
-'''
-Input: a List of integers
-Returns: a List of integers
-'''
+import math
+
+
 def product_of_all_other_numbers(arr):
-    # Your code here
+    result = []
 
-    pass
-
+    for i, num in enumerate(arr):
+        arr.remove(num)
+        product = math.prod(arr)
+        result.append(product)
+        arr.insert(i, num)
+    return result
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [1, 2, 3, 4, 5]
+    print(product_of_all_other_numbers(arr))
 
-    print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
+    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+
+    # print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
