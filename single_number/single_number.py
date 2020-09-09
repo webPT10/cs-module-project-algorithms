cee5bd7e7aa4d8b556d05d7cbc287e2f8625c013
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-09-08 17:20:52 -0700

    single_number > updated

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,20 +3,9 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    arr.sort()
-    compareList = []
-
-    if len(arr) == 0:
-        return None
-
-    for i, num in enumerate(arr): # if index is needed when running a four loop, enumerate()
-        if num in compareList:
-            compareList.remove(num)
-        else:
-            compareList.append(num)
-    
-    return compareList[0]
-
+   for i in arr:
+       if arr.count(i) == 1:
+           return i
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
